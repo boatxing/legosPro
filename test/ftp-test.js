27d723513b816c843466b6ded395b453cc19113a
xingzhizhou <xingzhizhou@jd.com>
2017-10-20 18:01:13 +0800

    log

--- test/ftp-test.js
+++ test/ftp-test.js
@@ -2,7 +2,8 @@
  * Created by user on 2017/10/19.
  */
 const ftpTest = require("../lib/ftpUtils");
+const path = require("path");
 
-ftpTest.put("log-test.js", "/home/boat/test/test/ftpUtils.js", ({code, msg}) => {
-    console.log(code, msg);
+ftpTest.put(path.join(__dirname, "/log-test.js"), "/export/wxsq/node_services/log-test.js", (err, result) => {
+
 })
