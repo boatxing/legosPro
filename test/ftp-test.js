e8d7b6d4df3af2a9b7b70faf8ec11326f7fd7d83
shipxing <434475464@qq.com>
2017-10-22 22:43:34 +0800

    sftp

--- test/ftp-test.js
+++ test/ftp-test.js
@@ -4,6 +4,8 @@
 const ftpTest = require("../lib/ftpUtils");
 const path = require("path");
 
-ftpTest.put(path.join(__dirname, "/log-test.js"), "/export/wxsq/node_services/log-test.js", (err, result) => {
-
+ftpTest.put(path.join(__dirname, "/log-test.js"), "/home/boat/test13/test1/log-t.est.js").then(({code, message}) => {
+    console.log(message)
+}).catch((e) => {
+    console.log(e.message);
 })
