e49a0e54e654ef021c193be6945cefd06c608a67
xingzhizhou <xingzhizhou@jd.com>
2017-10-24 20:12:50 +0800

    bin test

--- bin/legospro.js
+++ bin/legospro.js
@@ -10,9 +10,9 @@
 
  program.version("1.0.0");
 
- program.command("m.add <moduleName>")
-        .action((moduleName) => {
-            console.log('m.add %s', moduleName);
+ program.command("m.add <moduleName> <desc>")
+        .action((moduleName, desc) => {
+            console.log('m.add %s %s', moduleName, desc);
         });
 
 program.parse(process.argv);
